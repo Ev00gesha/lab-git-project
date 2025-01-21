6151c2e8577733109ed607610676e9bc484be4ef
Ev00gesha <rakzena180@gmail.com>
2025-01-21 23:09:41 +0300

    Добавление main

--- src/main.py
+++ src/main.py
@@ -4,3 +4,5 @@ from utils import add_numbers, multiply_numbers
 a, b = 5, 3
 print(f"Сумма: {add_numbers(a, b)}")
 print(f"Произведение: {multiply_numbers(a, b)}")
+
+print('Новая строка для 10 пункта')
